ba2b6c29a07973b083ed41fcb189f3fe846d8dbc
Steven Smith <github@icodeforyou.com>
2025-10-05 17:39:43 -0400

    Fixed abstract error

--- packages/nosql/lib/abstract/nosql_box_abstract.dart
+++ packages/nosql/lib/abstract/nosql_box_abstract.dart
@@ -3,6 +3,6 @@ import 'dart:async' show FutureOr;
 
 abstract class NoSqlBoxAbstract<T> {
   FutureOr<void> close();
-  FutureOr<void> put(dynamic key, T value);
+  FutureOr<bool> put(dynamic key, T value);
   FutureOr<T?> get(dynamic key, {T? defaultValue});
 }
